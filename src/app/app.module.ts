0f87006117bb506baa8e4c752f4ebb3fb0ffd7e8
bruno <brunolalpachino@gmail.com>
2022-07-02 09:29:06 +0300

    created the login and signup pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { SignInComponent } from './user/sign-in/sign-in.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     SignUpComponent,
-    SignInComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
