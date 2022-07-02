05b412a208a08154d9ee644f4c7682e9979f3c5d
bruno <brunolalpachino@gmail.com>
2022-07-02 08:44:42 +0300

    genereted the home sign up and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignUpComponent,
+    SignInComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
