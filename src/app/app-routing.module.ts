c15cb5169e7661a840b0db2c2193c5b4379cd44d
bruno <brunolalpachino@gmail.com>
2022-07-03 09:59:12 +0300

    created the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {SignUpComponent} from './sign-up/sign-up.component'
 import {HomeComponent} from './home/home.component'
 
 const routes: Routes = [
-  {path:"" , redirectTo:"login", pathMatch:"full"},
+  {path:"" , redirectTo:"home", pathMatch:"full"},
   {path:"login" , component:LoginComponent},
   {path:"signup", component:SignUpComponent},
   {path:"home", component:HomeComponent},
