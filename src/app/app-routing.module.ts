b0f68fd8d5889394d18f8a9727d1263516423ba7
bruno <brunolalpachino@gmail.com>
2022-07-04 20:08:01 +0300

    added the farmer routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {SignUpComponent} from './sign-up/sign-up.component'
 import {HomeComponent} from './home/home.component'
+import { FarmerComponent } from './farmer/farmer.component';
 
 const routes: Routes = [
   {path:"" , redirectTo:"home", pathMatch:"full"},
   {path:"login" , component:LoginComponent},
   {path:"signup", component:SignUpComponent},
   {path:"home", component:HomeComponent},
+  {path:"farmer", component:FarmerComponent}
 
 ];
 
