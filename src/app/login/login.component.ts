11441fb12b7b2cbe7ea18a72d53eb0f57fe6d4f3
bruno <brunolalpachino@gmail.com>
2022-07-04 20:11:24 +0300

    injected the login service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { FormGroup , FormControl, Validators} from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup!: FormGroup;
+  router: any;
 
-  ngOnInit(): void {
+  constructor(private loginService: LoginService){}
+  
+  ngOnInit(){
+    this.initForm();
+
   }
+  initForm(){
+    this.formGroup = new FormGroup({
+      username : new FormControl('',[ Validators.required]),
+      password : new FormControl('',[ Validators.required]),
+
+    });
+  }
+   LoginProcess(){
+     if(this.formGroup.valid){
+       this.loginService.loginUser(this.formGroup.value).subscribe(result=>{
+         if(result.success){
+           console.log(result);
+           alert(result.message)
+           this.router.navigate(['home'])
+         }
+         else{
+          console.log(result)
+          alert(result.message)
+
+         }
+       })
+     }
+
+   }
 
 }
