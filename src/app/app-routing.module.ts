91abba15d5d5220063874bc8e1c4089e7222826e
bruno <brunolalpachino@gmail.com>
2022-07-06 10:22:36 +0300

    added routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import {SignUpComponent} from './sign-up/sign-up.component'
 import {HomeComponent} from './home/home.component'
 import { FarmerComponent } from './farmer/farmer.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 const routes: Routes = [
   {path:"" , redirectTo:"home", pathMatch:"full"},
   {path:"login" , component:LoginComponent},
   {path:"signup", component:SignUpComponent},
   {path:"home", component:HomeComponent},
-  {path:"farmer", component:FarmerComponent}
+  {path:"farmer", component:FarmerComponent},
+  {path:"our-services", component:OurServicesComponent}
 
 ];
 
